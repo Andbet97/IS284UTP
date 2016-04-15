be703e4b7d0e3a4d3adaeeed6e712816dbb3b1be
UTP <utp@crie.local>
2016-04-15 08:58:52 -0500

    Ejercicioe ejemplo iniciado

---
+++ ejerciciosenclase/ejercicio_estructrura.cpp
@@ -0,0 +1,19 @@
+/*crear un estructura, crear una variable de tipo estructura y listar la estructura*/
+/*Librerias*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+/*variables globales*/
+
+typedef struct CODIGO_ASCII {
+  int n;
+  char c;
+};
+
+/*funciones prototipo*/
+
+CODIGO_ASCII *nuevo 
+
+/*funcion principal*/
